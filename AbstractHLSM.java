070018ed1246622105b836c1663976a42ca0738b
MichaelMGonzalez <mmg005@ucsd.edu>
2016-06-23 10:05:11 -0700

    Caught bugs in auto generated java code

--- AbstractHLSM.java
+++ AbstractHLSM.java
@@ -1,7 +1,20 @@
+import java.util.Calendar;
 public abstract class AbstractHLSM extends Thread {
-    protected abstract void runHLSM( long delayRate );
+	protected long delayRate = 100;
+    protected abstract void runHLSM( );
     public long transitionedAt = 0;
     public void run() {
-        
+        runHLSM( );
     }
+    protected void pause(long delayRate) {
+    	try {
+			Thread.sleep( delayRate );
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
+    protected long timeInState() {
+    	return Calendar.getInstance().getTimeInMillis() - transitionedAt;
+    }
+    
 }
