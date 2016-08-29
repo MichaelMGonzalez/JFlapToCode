f266eaec2822cb829ab72e95c885899e1b6f0581
Michael G <mmg005@ucsd.edu>
2016-08-29 12:37:06 -0400

    more abstractions to templating

--- templates/macros.jinja.cs
+++ templates/macros.jinja.cs
@@ -1,11 +1,55 @@
 #- macro cdf_transitions( transitions )
 #- for cdf, transition_state, probability in transitions: 
 // Probability of transition {{ probability * 100 }}%
-                        {%- if loop.first %}
+                        {%- if loop.length == 1 %}
+                        if( true )
+                        {%- elif loop.first %}
                         if( rand < {{cdf}} )
+                        {%- elif loop.last %}
+                        else
                         {%- else %}
                         else if( rand < {{cdf}} )
-                        {%- endif %}
+                        {% endif %}
                             state = State.{{transition_state}};
 #- endfor
 #- endmacro
+
+#- macro mdp_transition_structure( transition_set, continue )
+            #- for t_name, transition in transition_set.iteritems(): 
+            #- if transition.norm and transition.neg
+                    {%- if t_name %}
+                    if( {{t_name}}() ) {
+                    {%- else %}
+                    if( true ) {
+                    {%- endif %}
+                        {{cdf_transitions( transition.norm)}}
+                        {{should_continue( continue )}}
+                    }
+                    else {
+                        {{cdf_transitions( transition.neg)}}
+                        {{should_continue( continue )}}
+                    }
+                    {%- elif transition.norm %}
+                    {%- if t_name %}
+                    if( {{t_name}}() ) {
+                    {%- else %}
+                    if( true ) {
+                    {%- endif %}
+                        {{cdf_transitions( transition.norm)}}
+                        {{should_continue( continue )}}
+                    }
+                    {%- elif transition.neg%}
+                    if( !{{t_name}}() ) {
+                        {{cdf_transitions( transition.neg )}}
+                        {{should_continue( continue )}}
+                    }
+            #- endif 
+        #- endfor 
+#- endmacro
+
+#- macro should_continue( val )
+#- if val 
+continue;
+#- endif
+#- endmacro
+
