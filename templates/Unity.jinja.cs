c7b040103a2d7000b4fd0745399f816db30b3cea
Michael Gonzalez <mmg005@ucsd.edu>
2016-09-21 10:16:59 -0700

    Added Exception Handling to Unity State Machines

--- templates/Unity.jinja.cs
+++ templates/Unity.jinja.cs
@@ -1,8 +1,12 @@
 # import "macros.jinja.cs" as macros
 using UnityEngine;
+using System;
+using System.IO;
 using System.Collections;
 public abstract class {{ class_name }}AbstractFSM : MonoBehaviour, IStateMachine{
     protected float transitionedAt;
+    public int exceptionCount;
+    public int shutDownFSMAfterNExceptions = 10;
     # if delays
     # for state in delays
     public float delayDuring{{ state.name }} = {{state.delay}};
@@ -15,7 +19,8 @@ public abstract class {{ class_name }}AbstractFSM : MonoBehaviour, IStateMachine
     }  
     public State state = State.{{ init_state }};
     private IEnumerator FSMThread( float delayRate ) {
-        while(true) {
+        bool isRunning = true;
+        while(isRunning) {
             # if type == "mealy":
             // Get a uniform random number for MDP transitions
             float rand = Random.value;
@@ -27,31 +32,47 @@ public abstract class {{ class_name }}AbstractFSM : MonoBehaviour, IStateMachine
             # endif
             #- endif
             State prevState = state;
+            IEnumerator stateAction = null;
+            try {
             // The following switch statement handles the state machine's action logic
-            switch(state) {
+                switch(state) {
                 # for state in states:
                 # if state.has_func:
-                case State.{{state.name}}:
+                    case State.{{state.name}}:
                     # if state.func: 
-                    yield return Execute{{ state.func}}();
+                        stateAction = Execute{{ state.func}}();
                     # else:
-                    yield return ExecuteAction{{ state.name }}();
+                        stateAction = ExecuteAction{{ state.name }}();
                     # endif 
-                    break;
+                        break;
                 # endif 
                 # endfor 
+                }
             }
+            catch( Exception e ) {
+                LogException(e);
+            }
+            yield return stateAction;
             
             
+            try {
             # if type == "mealy" 
             # include "Unity_MDP.jinja.cs" 
             # else 
             # include "Unity_HLSM.jinja.cs" 
             # endif 
-
+            
+            }
+            catch(Exception e) {
+                LogException(e);
+            }
             yield return new WaitForSeconds( delayRate );
-
-            if ( prevState!=state ) {
+            if( exceptionCount > shutDownFSMAfterNExceptions )
+            {
+                Debug.LogError( this + " has exceeded the number of allowed exceptions! Shutting down.");
+                isRunning = false;
+            }
+            else if ( prevState!=state ) {
                 transitionedAt = Time.time;
                 OnTransition();
             }
@@ -89,6 +110,25 @@ public abstract class {{ class_name }}AbstractFSM : MonoBehaviour, IStateMachine
         bool rv = variable;
         variable = val;
         return rv;
+    }
+    protected void LogException(Exception e) {
+        string exceptionAcc = this + " threw exception " + e.GetType();
+        exceptionAcc += " while in " + state + "\n";
+        if( exceptionCount++ == 0 ) {
+            DateTime now = DateTime.Now;
+            string errorLogFileName = (Application.dataPath + "/Exceptions/");
+            errorLogFileName += now.Year + "_" + now.Month + "_" + now.Day + "/" + GetType() + "/";
+            errorLogFileName = errorLogFileName.Replace("/","\\");
+            try {
+                Directory.CreateDirectory(errorLogFileName);
+                errorLogFileName += DateTime.Now.ToFileTime() + "_" + "(" + e.GetType() + ").txt";
+                File.WriteAllText( errorLogFileName, exceptionAcc + e.StackTrace );
+                exceptionAcc += "Full details logged to: " + errorLogFileName + "\n";
+                exceptionAcc += e.StackTrace;
+            }
+            catch(Exception e2) { Debug.LogError("Could not create exceptions directory" + e2.GetType()); }
+        }
+        Debug.LogError( exceptionAcc );
     }
     protected virtual void OnTransition() { }
     public abstract void Reset();
